9bca090aec6a348be32b0bcd0272d2d58f184c3f
brunaze <brunodspinto98@gmail.com>
2023-03-02 14:47:54 +0100

    Navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -7,6 +9,8 @@ import { ClientComponent } from './client/client.component';
 import { ConnexionComponent } from './connexion/connexion.component';
 import { InscriptionComponent } from './inscription/inscription.component';
 import { ObjetComponent } from './objet/objet.component';
+import { MenuComponent } from './menu/menu.component';
+
 
 @NgModule({
   declarations: [
@@ -14,11 +18,14 @@ import { ObjetComponent } from './objet/objet.component';
     ClientComponent,
     ConnexionComponent,
     InscriptionComponent,
-    ObjetComponent
+    ObjetComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]

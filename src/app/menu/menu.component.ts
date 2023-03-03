41bb2b39e22cb083f920ba314db656c736a2770d
brunaze <brunodspinto98@gmail.com>
2023-03-03 16:07:13 +0100

    Formulaire v2

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,17 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit, ViewChild, } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from '../services/auth.service';
+
+declare var $: any;
 
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
   styleUrls: ['./menu.component.css']
 })
+
 export class MenuComponent implements OnInit {
 
-  constructor(private route: Router, public authService: AuthService) { }
+  @ViewChild('closebutton') closebutton: any;
+
+  user: any;
+  msgErr: any;
+  constructor(private route: Router, public authService: AuthService, private http: HttpClient) { }
 
   ngOnInit(): void {
 
+  }
+
+  connexion(val: any) {
+    this.http.post('http://localhost:8283/utilisateur/login', val).subscribe({
+      next: (data) => {
+        this.user = data;
+        if (this.user != null) {
+          this.authService.setUserConnect(this.user);
+        } else {
+          this.msgErr = 'Identifiant ou mot de passe incorrect';
+        }
+      },
+      error: (err) => { console.log(err) }
+    })
   }
 }

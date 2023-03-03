b19f7d011e1f457af951350ab9d4de141069c786
LiamLeGoffic <liamlegoffic@yahoo.fr>
2023-03-03 10:23:49 +0100

    test liam

--- src/app/objet/objet.component.ts
+++ src/app/objet/objet.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-objet',
   templateUrl: './objet.component.html',
   styleUrls: ['./objet.component.css']
 })
-export class ObjetComponent {
+export class ObjetComponent implements OnInit {
 
+  constructor() { }
+
+  ngOnInit(): void {
+
+  }
 }

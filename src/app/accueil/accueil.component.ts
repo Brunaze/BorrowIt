f0a9269039c8a466acaa6a8c8c95684e93d45933
Brunaze <brunodspinto98@gmail.com>
2023-03-05 16:17:20 +0100

    Inscription + accueil

--- src/app/accueil/accueil.component.ts
+++ src/app/accueil/accueil.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-accueil',
   templateUrl: './accueil.component.html',
   styleUrls: ['./accueil.component.css']
 })
-export class AccueilComponent {
+export class AccueilComponent implements OnInit {
+
+  objet: any;
+  constructor(private http: HttpClient, private route: Router) { }
+
+  ngOnInit(): void {
+    this.http.get('http://localhost:8283/objet').subscribe({
+      next: (data) => { this.objet = data },
+      error: (err) => { console.error(err) }
+    })
+  }
 
 }

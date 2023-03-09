775fddc9a53f778a92408e06c2096ef74f855181
Brunaze <brunodspinto98@gmail.com>
2023-03-09 16:45:42 +0100

    signal

--- src/app/administrateur/administrateur.component.ts
+++ src/app/administrateur/administrateur.component.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from '../services/auth.service';
+import { ListeSignalementService } from '../services/liste-signalement.service';
 
 @Component({
   selector: 'app-administrateur',
@@ -11,10 +12,15 @@ import { AuthService } from '../services/auth.service';
 export class AdministrateurComponent implements OnInit {
 
   users: any;
-  constructor(private http: HttpClient, public route: Router, public authService: AuthService) { }
+  nbUsers: any;
+  signalements: any;
+  signalementsById: any;
+  nbSignalement: any;
+  constructor(private http: HttpClient, public route: Router, private signalementService: ListeSignalementService) { }
 
   ngOnInit(): void {
     this.allUsers();
+    this.allSignalements();
   }
 
   allUsers() {
@@ -22,6 +28,40 @@ export class AdministrateurComponent implements OnInit {
       next: (data) => {
         console.log(data);
         this.users = data;
+      },
+      error: (err) => { console.log(err) }
+    })
+  }
+
+
+
+
+
+  allSignalements() {
+    this.http.get('http://localhost:8283/signalement').subscribe({
+      next: (data) => {
+        this.signalements = data;
+        return this.signalements;
+      },
+      error: (err) => { console.log(err) }
+    })
+  }
+
+  signalementById(id: any) {
+    this.http.get('http://localhost:8283/signalement/listeSignalementClient/' + id).subscribe({
+      next: (data) => {
+        this.signalementsById = data;
+        return this.signalementsById;
+      },
+      error: (err) => { console.log(err) }
+    })
+  }
+
+  getNbSignalement(id: any) {
+    this.http.get('http://localhost:8283/signalement/nbSignalements/' + id).subscribe({
+      next: (data) => {
+        this.nbSignalement = data;
+        return this.nbSignalement;
       },
       error: (err) => { console.log(err) }
     })
